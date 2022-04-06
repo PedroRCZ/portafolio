5a2a19213d4f1635ee71e482df041ecb55549416
PedroRCZ <pedro400012@gmail.com>
2022-04-05 20:20:19 -0500

    Cursor personalizado

--- js/javascript.js
+++ js/javascript.js
@@ -1,26 +1,25 @@
+let innerCursor = document.querySelector(".inner-cursor");
+let outerCursor = document.querySelector(".outer-cursor");
 
-function init() {
-    const ipad= window.matchMedia('screen and (max-width: 767px)')
+document.addEventListener("mousemove", moveCursor);
 
-    const menu = document.querySelector('.menu');
-    const burgerButton = document.querySelector('#burger-menu');
-    ipad.addListener(validation);
-
-    function validation(event){
-        if(event.matches){
-            burgerButton.addEventListener('click', hideShow);
-        }else{
-            burgerButton.removeEventListener('click', hideShow);
-        }
-    }
-
-    function hideShow(){
-        if(menu.classList.contains('is-active')){
-            menu.classList.remove('is-active');
-        }else{
-            menu.classList.add('is-active');
-        }
-    }
+function moveCursor(e){
+    let x = e.clientX;
+    let y = e.clientY;
+    innerCursor.style.left = `${x}px`;
+    innerCursor.style.top = `${y}px`;
+    outerCursor.style.left = `${x}px`;
+    outerCursor.style.top = `${y}px`;
 }
 
-document.addEventListener('DOMContentLoaded', init);
+let links = Array.from(document.querySelectorAll("a"));
+console.log(links)
+
+links.forEach((link) =>{
+    link.addEventListener("mouseover", () =>{
+        innerCursor.classList.add("grow");
+    });
+    link.addEventListener("mouseleave", () =>{
+        innerCursor.classList.remove("grow");
+    });
+})
