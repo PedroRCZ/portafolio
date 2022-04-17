8fe9cc9583d43f1ae4b5c031f3736b246a290e18
PedroRCZ <pedro400012@gmail.com>
2022-04-17 15:38:54 -0500

    Nav

--- js/javascript.js
+++ js/javascript.js
@@ -49,3 +49,12 @@ function hideShow(){
         menu.classList.add('is-active');
     }
 }
+
+const list = document.querySelectorAll('.list');
+function activeLink(){
+    list.forEach((item) =>
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+item.addEventListener('click',activeLink));
